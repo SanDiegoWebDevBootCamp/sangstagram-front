37c4554580fe13ac9586d0b03a73491f93627f6f
Sang Yum <sangyum@gmail.com>
2019-11-25 19:15:28 -0800

    added grapqhl-enabled component

---
+++ src/Books.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+const BOOKS = gql`
+    {
+        books {
+            title
+            author
+        }
+    }
+`;
+
+
+function Books() {
+  const { loading, error, data } = useQuery(BOOKS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.books.map(({ title, author }) => (
+    <div key={title}>
+      <p>
+        {title}: {author}
+      </p>
+    </div>
+  ));
+}
+
+export default Books
---
+++ src/graphql/index.js
@@ -0,0 +1,10 @@
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+    // TODO: Use environment variable
+  uri: 'http://localhost:5000/graphql',
+});
+
+export {
+    client
+}
