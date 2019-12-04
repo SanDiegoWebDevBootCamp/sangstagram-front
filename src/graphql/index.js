a06de805e188c201d1d45562d375384f12997a58
Sang Yum <sangyum@gmail.com>
2019-12-03 22:29:49 -0800

    use environment variable for backend

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -2,7 +2,7 @@ import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
     // TODO: Use environment variable
-    uri: 'http://localhost:5000/graphql',
+    uri: process.env.BACKEND_URL,
 });
 
 export default client;
