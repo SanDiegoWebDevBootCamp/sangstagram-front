fbe5735726a003af59c4d569642e342e2bbcb20d
Sang Yum <sangyum@gmail.com>
2019-12-15 21:17:19 -0800

    refactored Navigation

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -5,19 +5,10 @@ import {
     Route,
 } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import LinkTab from './LinkTab';
+import NavBar from './NavBar';
 import Posts from '../Posts';
 import Following from '../Following';
 import Profile from '../Profile';
-
-function a11yProps(index) {
-    return {
-        id: `nav-tab-${index}`,
-        'aria-controls': `nav-tabpanel-${index}`,
-    };
-}
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -28,27 +19,10 @@ const useStyles = makeStyles((theme) => ({
 
 function Navigation() {
     const classes = useStyles();
-    const [value, setValue] = React.useState(0);
-
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
-
     return (
         <Router>
             <div className={classes.root}>
-                <AppBar position="static">
-                    <Tabs
-                        variant="fullWidth"
-                        value={value}
-                        onChange={handleChange}
-                        aria-label="nav tabs example"
-                    >
-                        <LinkTab label="Posts" href="/" {...a11yProps(0)} />
-                        <LinkTab label="Following" href="/following" {...a11yProps(1)} />
-                        <LinkTab label="Profile" href="/profile" {...a11yProps(2)} />
-                    </Tabs>
-                </AppBar>
+                <NavBar />
                 <Switch>
                     <Route exact path="/">
                         <Posts />
