d32ac1a0bef01779dd0ab020523f3fb9e78cf6e5
Brandon Yum <byum1996@gmail.com>
2019-12-17 22:21:58 -0800

    Added button to Following

--- src/Following/FollowingButton.js
+++ src/Following/FollowingButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button } from '@material-ui/core';
 
 const FollowingButton = () => (
-    <Button>Following</Button>
+    <Button variant="outlined" color="secondary">Following</Button>
 );
 
 export default FollowingButton;
