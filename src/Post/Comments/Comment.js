f69e29bbbd27b307bc996529f365b4c6589ba8af
Brandon Yum <byum1996@gmail.com>
2019-12-19 19:06:49 -0800

    LikesandComments

--- src/Post/Comments/Comment.js
+++ src/Post/Comments/Comment.js
@@ -3,7 +3,6 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
-// import { borders } from '@material-ui/system';
 import Box from '@material-ui/core/Box';
 
 
