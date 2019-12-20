bca0185b754ed8ad48264bd06cbf048163edce76
SungMin <sumimoonfilegather@gmail.com>
2019-12-19 20:22:59 -0800

    started on the sidebar navigation but will need to be worked on

---
+++ src/stories/SidebarNavigation.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import SidebarNavigation from '../SidebarNavigation/index';
+
+export default {
+    title: 'SidebarNavigation',
+};
+
+export const Navigation = () => <SidebarNavigation />;
